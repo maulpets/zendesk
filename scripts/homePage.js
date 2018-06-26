3c463e8335b490c6d9525557977d302e65a8bbc1
maulpets <maulpets@gmail.com>
2018-06-26 15:11:01 -0400

    navigation bar

---
+++ scripts/homePage.js
@@ -0,0 +1,28 @@
+var last_known_scroll_position = 0;
+var ticking = false;
+
+function doSomething(scroll_pos) {
+  if(scroll_pos > 0){
+    $('.header-category').removeClass('hidden');
+  }
+  else {
+    $('.header-category').addClass('hidden');
+  }
+}
+
+window.addEventListener('scroll', function(e) {
+
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+
+  }
+
+});
